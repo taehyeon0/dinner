efd47af50ab0f58fc3833ebdf826fb9de42c0fab
taehyeon0 <taehyeon0.an@gmail.com>
2022-11-08 22:50:29 +0900

    initial commit

---
+++ src/test/java/ka/dinner/DinnerApplicationTests.java
@@ -0,0 +1,13 @@
+package ka.dinner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
